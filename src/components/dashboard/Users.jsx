b68bba7430c442a9cb41fef0162395000336772d
ikrom2008 <77ikromjonbaxodirov77@gmail.com>
2024-05-20 13:45:43 +0500

    ds

--- src/components/dashboard/Users.jsx
+++ src/components/dashboard/Users.jsx
@@ -42,7 +42,7 @@ function Users({menu}) {
   return (
     <div>
       <ul className='lists'>
-        <li className='list' style={menu ? {position: 'fixed', paddingRight: '320px'} : {position: 'fixed', paddingRight: '80px'} }>
+        <li className='list' style={menu ? {position: 'fixed', paddingRight: '270px'} : {position: 'fixed', paddingRight: '80px'} }>
             <div >
                 <h4 style={{opacity: '0.6'}}>Ticket details</h4>
             </div>
